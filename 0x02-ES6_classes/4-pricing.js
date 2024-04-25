41b75b2e77c5d9c94fc8030c80d1d7c281363b25
mouadedd <mdx.edd@gmail.com>
2024-04-25 04:53:46 -0700

    test

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    if (amount === 'number') {
+      this._amount = amount;
+    }
+  }
+
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
