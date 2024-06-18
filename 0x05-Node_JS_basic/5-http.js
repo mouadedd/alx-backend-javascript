fd5f248dcf5b134af27a5306ce8ffab4bd97334d
mouadedd <mdx.edd@gmail.com>
2024-06-18 17:26:37 +0000

    5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const { readFile } = require('fs');
+const countStudents = require('./2-read_file');
+
+const host = 'localhost';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString()).then((output) => {
+      const outString = output.slice(0, -1);
+      res.end(outString);
+    }).catch(() => {
+      res.statusCode = 404;
+      res.end('Cannot load the database');
+    });
+  }
+});
+
+app.listen(port, host, () => {
+});
+
+module.exports = app;
